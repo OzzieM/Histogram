aa6eb89c4d6d6b9d7072e5263a5c73e57aabcec6
OzzieM <54642856+OzzieM@users.noreply.github.com>
2019-09-17 08:03:57 -0700

    Add files via upload

--- Histogram.java
+++ Histogram.java
@@ -14,48 +14,46 @@ import java.util.stream.Stream;
 
 public class Histogram{
 
-    //sorting the Histogram
+    //sort the Histogram
     private static void Bubble(int[] Hist){
-
             int n = Hist.length;
-            for (int i = 0; i < n-1; i++)
-                for (int j = 0; j < n-i-1; j++)
-                    if ( Hist[j] >  Hist[j+1])
-                    {
-                        int temp =  Hist[j];
-                        Hist[j] = Hist[j+1];
-                        Hist[j+1] = temp;
+            for (int i = 0; i < n-1; i++){
+                for (int j = 0; j < n - i - 1; j++)
+                    if (Hist[j] > Hist[j + 1]) {
+                        int temp = Hist[j];
+                        Hist[j] = Hist[j + 1];
+                        Hist[j + 1] = temp;
                     }
+            }
     }
-
     public static void main(String[] args) {
 
         //initialize and give a value for A-K
         Map<String, Integer> abc = new HashMap<>();
-        abc.put("A",0);
-        abc.put("B",0);
-        abc.put("C",0);
-        abc.put("D",0);
-        abc.put("E",0);
-        abc.put("F",0);
-        abc.put("G",0);
-        abc.put("H",0);
-        abc.put("I",0);
-        abc.put("J",0);
-        abc.put("K",0);
+            abc.put("A",0);
+            abc.put("B",0);
+            abc.put("C",0);
+            abc.put("D",0);
+            abc.put("E",0);
+            abc.put("F",0);
+            abc.put("G",0);
+            abc.put("H",0);
+            abc.put("I",0);
+            abc.put("J",0);
+            abc.put("K",0);
 
 
-        StringBuilder test = new StringBuilder();
         //take user input for txt file
         Scanner a = new Scanner(System.in);
-        System.out.print("Enter filename:");
-        String filename = a.nextLine();
+            System.out.print("Enter filename:");
+            String filename = a.nextLine();
 
         File f = new File(filename);
-        Scanner scan = null;
+            Scanner scan = null;
         try{
             scan = new Scanner(f);
-        }catch (FileNotFoundException e){
+        }
+        catch (FileNotFoundException e){
             System.out.println(filename + " file not found");
         }
 
@@ -63,7 +61,7 @@ public class Histogram{
         int count = 0;
         while(scan.hasNext()){
             String input = scan.nextLine();
-            test.append(input);
+            //test.append(input);
             if(!abc.containsKey(input)){ //thought we were reading from user input, rather than file so this may be useless now.
                 abc.put(input, 1);
                 count++;
@@ -71,20 +69,19 @@ public class Histogram{
             else if(abc.containsKey(input)){
                 abc.put(input, abc.get(input) + 1);
             }
-
         }
         //create array to store Hashmap values
-        int increment = 1;
-        int[] Hist = new int[12];
-        for (String i : abc.keySet()){
-            Hist[increment++] = abc.get(i);
-        }
+        int increment = 1; //left Hist[0] empty so I could put the number column + creates some more space
+            int[] Hist = new int[12];
+            for (String i : abc.keySet()){
+                Hist[increment++] = abc.get(i);
+            }
         //records max height for histogram
         int max =0;
-        for(int i = 0; i<Hist.length; i++){
-            if(Hist[i] > max)
-                max=Hist[i];
-        }
+            for(int i = 0; i<Hist.length; i++){
+                if(Hist[i] > max)
+                    max=Hist[i];
+            }
         //sort from least to greatest
         Bubble(Hist);
 
@@ -102,7 +99,6 @@ public class Histogram{
             }//new row line
             System.out.println();
         }
-
         //tried ascii but figured it'd be easier to sort abc map and display only the letters
 //        for(int i=65;i<=75;i++)
 //            System.out.print(Character.toString((char)i)+" ");
@@ -115,16 +111,15 @@ public class Histogram{
 //            }
 //
 //        }
-
         //sorts the abc map so that I can print out the keys/letters only
         System.out.println("===========================");
         System.out.print("|NO|");
-       abc.entrySet().stream()
+        abc.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed().reversed()) //reverse twice to sort least to greatest
                 .limit(Hist.length).forEachOrdered(i -> System.out.print(" "+i.getKey()));
                                                     //^^^ first time doing this so it was annoying to figure out,
                                                     //i.getKey() solved everything though
         System.out.println();
-        System.out.println("===========================");
+            System.out.println("===========================");
     }
 }
